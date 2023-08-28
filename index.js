fa2f20f849c8f1bb0eb24548284f864e0d27b2f2
TANMAY SHARMA <123546871+cbof16@users.noreply.github.com>
2023-08-28 17:46:58 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fullRate=document.querySelectorAll('.rating .non')
 const button=document.querySelector('.button')
-const content=document.querySelector('.content')
+const content=document.querySelector('.box')
 
 fullRate.forEach((item,idx)=>
 {
@@ -14,14 +14,24 @@ fullRate.forEach((item,idx)=>
             
                 button.addEventListener('click', (e) => {
                     content.innerHTML = `
+                        <Link rel="stylesheet" href="submitted.css">
+                        <img src="illustration-thank-you.svg" class>
                         
-                        Thank You!
+                        <div class="submit_rating">
+                        <span class="text_submit">
                         You selected ${i+1} out of 5
+                        </span>
+                        </div>
+                        <h2>
+                        Thank You!
+                        </h2>
+                        <p>
                         We appreciate you taking the time to give a rating.<br>
                         if you ever need more support,don't hesitate to get<br>
                         in touch.
-    
+                        </p>
                     `
+
                 })
             }
             
